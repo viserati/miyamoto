045522bd1c3a07d6387eeaa11f85b37835feda28
Keith Turner <turner@keithturner.com>
2018-06-25 20:47:11 -0600

    Fixed: Controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # Fixed Controller
+  # Fixed Controller.
   protect_from_forgery with: :exception
 end
